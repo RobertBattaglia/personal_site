9513628a4e44e3c6139a1a84a3616030517e7bae
Robert Battaglia <r083rt164@gmail.com>
2019-10-10 02:16:58 -0400

    fix issue where button was submitting form improperly

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,16 +9,14 @@ const Wrapper = styled("section")`
   padding-top: 2rem;
 `
 
-const Form = styled("form")`
-  display: flex;
-  flex-direction: column;
+const FormWrapper = styled("div")`
   margin: 0 auto;
   padding: 0 1rem 2rem;
   width: 540px;
+  text-align: center;
   border: solid 3px ${theme.primaryColor};
   border-radius: 3rem;
   font-size: 2rem;
-  text-align: center;
   background-color: #fff;
   box-shadow: -0.5rem -0.5rem 0.7rem rgba(0, 0, 0, 0.6);
 
@@ -27,19 +25,28 @@ const Form = styled("form")`
   }
 `
 
+const Form = styled("form")`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+`
+
 const Label = styled("label")`
-  display: inherit;
-  padding-left: 0.5rem;
+  padding: 0.5rem 0rem;
   font-size: 1.5rem;
 `
 
 const Input = styled("input")`
+  display: block;
+  width: 100%;
   border: solid #e6ecf8 1px;
   border-radius: 1rem;
   font-size: 1.25rem;
 `
 
 const Textarea = styled("textarea")`
+  display: block;
+  width: 100%;
   border: solid #e6ecf8 1px;
   border-radius: 1rem;
   font-size: 1.25rem;
@@ -52,22 +59,31 @@ const SubmitButton = styled(Button)`
 const Contact = () => {
   return (
     <Wrapper id="contact">
-      <Form
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
+      <FormWrapper>
         <h2>Contact Me</h2>
         <p>I'd love to hear from you!</p>
-        <Label>Email</Label>
-        <Input type="email" name="email" />
-        <Label>Name</Label>
-        <Input type="text" name="name" />
-        <Label>Message</Label>
-        <Textarea rows={4} />
-        <SubmitButton type="submit">Send Message</SubmitButton>
-      </Form>
+        <Form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="bot-field" />
+          <Label>
+            Email
+            <Input type="email" name="email" />
+          </Label>
+          <Label>
+            Name
+            <Input type="text" name="name" />
+          </Label>
+          <Label>
+            Message
+            <Textarea name="message" rows={4} />
+          </Label>
+          <SubmitButton type="submit">Send Message</SubmitButton>
+        </Form>
+      </FormWrapper>
     </Wrapper>
   )
 }
