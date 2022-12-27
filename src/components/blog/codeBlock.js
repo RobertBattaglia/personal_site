7f951d7edb97d70f7d9e25625be9331e7bc672c4
Robert Battaglia <r083rt164@gmail.com>
2022-12-27 14:04:34 -0500

    add support youtube embed

--- src/components/blog/codeBlock.js
+++ src/components/blog/codeBlock.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import { base16AteliersulphurpoolLight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import styled from "@emotion/styled";
+
+const YoutubeContainer = styled("div")`
+  position: relative;
+  width: 100%;
+  height: 315px;
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 const CodeBlock = ({ lang, content }) => {
   const [isMounted, setIsMounted] = useState(false);
@@ -13,7 +28,11 @@ const CodeBlock = ({ lang, content }) => {
     return content;
   }
 
-  if (lang === "dangerouslysetinnerhtml") {
+  if (lang === "youtube") {
+    return <YoutubeContainer dangerouslySetInnerHTML={{ __html: content }} />;
+  }
+
+  if (lang === "dangerouslysetinnerhtml" || lang === "youtube") {
     return <span dangerouslySetInnerHTML={{ __html: content }} />;
   }
 
